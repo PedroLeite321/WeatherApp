7fae0ac63169215d34d698c51ebc81e1b5cc1028
Pedro Kauã <pk.bleite@gmail.com>
2023-02-02 14:00:56 -0300

    simple page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+const api_info = {
+  key: '72095579eb8b2c9381a1d588e381fcbe',
+  httpRequire: 'https://api.openweathermap.org/data/2.5/weather'
+}
+const current = new Date()
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='searchCity' >
+          <input type="text" className="searchBar" placeholder="Search City"/>
+        </div>
+        <div className="place">
+          <h2>Tatuí, Br</h2>
+        </div>
+        <div className="dates">
+          <h2>{current.getDate()}/{current.getMonth()+1}/{current.getFullYear()}</h2>
+        </div>
+        <div className='temperature'></div>
+      </main>
     </div>
   );
 }
