062eeef5519052f5628fbe4cdb25c6f963e8cdaf
Pedro Kauã <pk.bleite@gmail.com>
2023-02-12 22:29:15 -0300

    more_dynamicApp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-const api_info = {
-  key: '72095579eb8b2c9381a1d588e381fcbe',
-  httpRequire: 'https://api.openweathermap.org/data/2.5/weather'
-}
-const current = new Date()
+import { useState } from 'react';
+
 function App() {
+  const apiKey = '72095579eb8b2c9381a1d588e381fcbe';
+  let citySearch;
+  const current = new Date();
+  let temperature = "";
+  let [weather, setWeather] = useState();
+  const startFetch = async (e) =>{
+    if(e.key === "Enter") {
+      citySearch = e.target.value;
+      console.log(citySearch.length);
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&APPID=${apiKey}`)
+      .then(function(resp){return resp.json()})//transfom the data from fetch into json.
+      .then(data => {
+        setWeather(data);
+      })//rename resp.json to data.  
+      .catch(Error)
+    }
+  }
+   if(weather !== undefined){
+      temperature = weather.main.temp
+      function turnToCelcius(turnToC) {
+        turnToC = temperature - 273.15;
+        console.log(turnToC)
+        return temperature = Math.round(turnToC);
+      }
+      turnToCelcius(temperature);
+    } 
   return (
     <div className="App">
       <main>
         <div className='searchCity' >
-          <input type="text" className="searchBar" placeholder="Search City"/>
+          <input type="text" className="searchBar" placeholder="Search City" onKeyUp={startFetch} />
         </div>
         <div className="place">
-          <h2>Tatuí, Br</h2>
+          <h2>{weather === undefined? "" : weather.name}</h2>
         </div>
         <div className="dates">
-          <h2>{current.getDate()}/{current.getMonth()+1}/{current.getFullYear()}</h2>
+          <h2>{current.getUTCDate()}/{current.getUTCMonth()+1}/{current.getUTCFullYear()}</h2>
+          <h3>{current.getHours()}:{current.getMinutes() < 10? '0' + current.getMinutes() : current.getMinutes()}:{current.getSeconds() < 10? '0' + current.getSeconds() : current.getSeconds()}</h3>
         </div>
-        <div className='temperature'></div>
+        <div className='temperature'>
+          <p className="temperatureDisplay">{weather === undefined? "" : temperature !== "" ? temperature + "ºC" : ""}</p>
+        </div>
       </main>
     </div>
   );
 }
 
 export default App;
+
+function Api()  {
+
+}
+Api()
