d7353671dcf428204e91817ac427b15c8564956a
Pedro Kauã <pk.bleite@gmail.com>
2023-04-18 23:04:04 -0300

    minorChanges

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const apiKey = '72095579eb8b2c9381a1d588e381fcbe';
-  let citySearch;
+  let citySearch = "";
   const current = new Date();
   let temperature = "";
   let [weather, setWeather] = useState();
   const startFetch = async (e) =>{
+
     if(e.key === "Enter") {
+      if(e.target.value === ""){
+        e.target.value = "london";
+      }
       citySearch = e.target.value;
-      console.log(citySearch.length);
       await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&APPID=${apiKey}`)
       .then(function(resp){return resp.json()})//transfom the data from fetch into json.
       .then(data => {
@@ -19,17 +21,46 @@ function App() {
       })//rename resp.json to data.  
       .catch(Error)
     }
+
   }
+
    if(weather !== undefined){
       temperature = weather.main.temp
       function turnToCelcius(turnToC) {
         turnToC = temperature - 273.15;
-        console.log(turnToC)
         return temperature = Math.round(turnToC);
       }
       turnToCelcius(temperature);
     } 
+  if(weather !== undefined){
+    const climate = weather.weather[0].main;
+    return (
+
+      <div className="App">
+        <main>
+          <div className='searchCity' >
+            <input type="text" className="searchBar" placeholder="Search City" onKeyUp={startFetch} />
+          </div>
+          <div className="place">
+            <h2>{weather === undefined? "" : weather.name}</h2>
+          </div>
+          <div className="dates">
+            <h2>{current.getUTCDate()}/{current.getUTCMonth()+1}/{current.getUTCFullYear()}</h2>
+            <h3>{current.getHours()}:{current.getMinutes() < 10? '0' + current.getMinutes() : current.getMinutes()}:{current.getSeconds() < 10? '0' + current.getSeconds() : current.getSeconds()}</h3>
+          </div>
+          <div>
+            <h2>{climate}</h2>
+          </div>
+          <div className='temperature'>
+            <p className="temperatureDisplay">{weather === undefined? "" : temperature !== "" ? temperature + "ºC" : ""}</p>
+          </div>
+        </main>
+      </div>
+
+    );
+}else{
   return (
+    
     <div className="App">
       <main>
         <div className='searchCity' >
@@ -47,12 +78,9 @@ function App() {
         </div>
       </main>
     </div>
+
   );
+}
 }
 
 export default App;
-
-function Api()  {
-
-}
-Api()
